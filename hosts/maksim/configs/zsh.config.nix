a753524d298ee3d77b6d125cd020a7c12a88aa31
Maksim Antonov <karnalize@mail.ru>
2026-06-02 23:23:16 +0500

    Обновлен алиас для консоли

--- hosts/maksim/configs/zsh.config.nix
+++ hosts/maksim/configs/zsh.config.nix
@@ -7,10 +7,13 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
+      # Nix
       nix-rebuild = "sudo nixos-rebuild switch --flake .#maksim";
       nix-rebuild-test = "sudo nixos-rebuild test --flake .#maksim";
       nix-gens = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
       nix-gens-del = "sudo nix-collect-garbage -d";
+      # Console
+      cls = "clear";
     };
   };
 }
