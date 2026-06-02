2fe06ebbacb002a6134a539ccc87ccd7ffaddf3a
Maksim Antonov <karnalize@mail.ru>
2026-06-02 23:16:27 +0500

    Обновлена конфигурация zsh

--- hosts/maksim/configs/zsh.config.nix
+++ hosts/maksim/configs/zsh.config.nix
@@ -2,10 +2,14 @@
 {
   programs.zsh = {
     enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
 
     shellAliases = {
       nix-rebuild = "sudo nixos-rebuild switch --flake .#maksim";
       nix-rebuild-test = "sudo nixos-rebuild test --flake .#maksim";
+      nix-gens = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
+      nix-gens-del = "sudo nix-collect-garbage -d";
     };
   };
 }
