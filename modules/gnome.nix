d039a4962c8f6ae01880daea5db3829ff156108c
Maksim Antonov <karnalize@mail.ru>
2026-06-28 20:51:07 +0500

    Убрана настройка для отпечатка пальца

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 {
   # Включение службы fprintd для работы со сканером
-  services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  # Драйвер
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  # Не работает для Honor
+  services.fprintd.enable = false;
+  services.fprintd.tod.enable = false;
+  # Драйвер (В Honor нет доступного драйвера)
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
   # Использование отпечатка пальца для подтверждения команд sudo в терминале
-  security.pam.services.sudo.fprintAuth = true;
+  security.pam.services.sudo.fprintAuth = false;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
