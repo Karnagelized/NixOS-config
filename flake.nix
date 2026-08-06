581c211152c47b89a69becb17b92088d67184e40
Antonov Maksim <karnalize@mail.ru>
2026-08-06 19:19:43 +0500

    Обновлена версия пакетного менеджера nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
 	description = "Кастомный Flake для Maksim NixOS";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
         home-manager = {
-          url = "github:nix-community/home-manager/release-25.11";
+          url = "github:nix-community/home-manager/release-26.05";
           inputs.nixpkgs.follows = "nixpkgs";
         };
 
     # AAGL Для лаунчера Genshin Impact
-    aagl.url = "github:ezKEa/aagl-gtk-on-nix";
+    aagl.url = "github:ezKEa/aagl-gtk-on-nix/release-26.05";
     aagl.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
