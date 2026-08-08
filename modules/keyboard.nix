baac39e4fb45e11281414bc6d58fff385867114a
Antonov Maksim <karnalize@mail.ru>
2026-08-08 20:25:40 +0500

    Улучшена система переключений между окнами

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -9,4 +9,19 @@
     variant = "";
     options = "grp:win_space_toggle";
   };
+
+  # Переключение между приложениями и окнами
+  # только внутри рабочего пространства
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/shell/app-switcher" = {
+          current-workspace-only = true;
+        };
+        "org/gnome/shell/window-switcher" = {
+          current-workspace-only = true;
+        };
+      };
+    }
+  ];
 }
