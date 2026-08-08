0fc149058bbbe8a30d370db0b36a2e12a6560e47
Antonov Maksim <karnalize@mail.ru>
2026-08-08 15:10:34 +0500

    Исправлена конфигурация переключений раскладки

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -2,7 +2,7 @@
 {
   # Конфигурация переключений раскладки в X11
   services.xserver.xkb = {
-    layout = "ru,us";
+    layout = "us,ru";
     variant = "";
     options = "grp:win_space_toggle";
   };
