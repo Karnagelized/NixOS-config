1d58285160d10a97173b04446edffd10283f2fe3
Antonov Maksim <karnalize@mail.ru>
2026-08-08 21:10:22 +0500

    Улучшена система перемещений между окнами

--- hosts/maksim/configs/gnome-binds.config.nix
+++ hosts/maksim/configs/gnome-binds.config.nix
@@ -12,6 +12,31 @@ in
   dconf.enable = true;
 
   dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      # Alt+Tab: individual windows on the current workspace.
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+
+      # Super+Tab: standard GNOME application switcher across workspaces.
+      switch-applications = [ "<Super>Tab" ];
+      switch-applications-backward = [ "<Shift><Super>Tab" ];
+    };
+
+    "org/gnome/shell/window-switcher" = {
+      current-workspace-only = true;
+      app-icon-mode = "both";
+    };
+
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = false;
+    };
+
+    "org/gnome/shell/extensions/tilingshell" = {
+      # Tiling Shell adds a "tiled windows" group into Alt+Tab by default.
+      # Keep Alt+Tab as GNOME's normal window switcher instead.
+      override-alt-tab = false;
+    };
+
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
