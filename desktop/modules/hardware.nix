4ef5dcd5d59d9af4fa40387f8c44a61261d9276e
Antonov Maksim <karnalize@mail.ru>
2026-08-12 20:15:21 +0500

    Обновлен nvidia модуль

--- desktop/modules/hardware.nix
+++ desktop/modules/hardware.nix
@@ -1,16 +1,45 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      libvdpau-va-gl
+      libva-vdpau-driver
+    ];
   };
 
   # Для Nvidia карт
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.nvidiaSettings = true;
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.open = true;
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    powerManagement.enable = true;
+
+    # Для GTX 10xx и старше нужно заменить на false.
+    open = false;
+  };
+
+  boot.kernelParams = [
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+  ];
+
+  environment.sessionVariables = {
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "nvidia";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vulkan-tools
+    libva-utils
+    nvtopPackages.nvidia
+  ];
 
   # Для AMD карт
   # services.xserver.videoDrivers = [ "amdgpu" ];
