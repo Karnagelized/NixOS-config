caef5fadd1473f5cc5feaf87db194745dcd80038
Antonov Maksim <karnalize@mail.ru>
2026-08-12 18:52:56 +0500

    Закончена предварительная версия верхнего меню

--- desktop/configs/eww/bar/scripts/bluetooth.sh
+++ desktop/configs/eww/bar/scripts/bluetooth.sh
@@ -2,21 +2,34 @@
 
 get_status() {
     if rfkill list bluetooth | grep -q "Soft blocked: yes"; then
-        echo "1" # Иконка выключенного BT (FontAwesome / Nerd Fonts)
+        echo "󰂲" # Выключен
     elif bluetoothctl show | grep -q "Powered: yes"; then
-        if bluetoothctl info | grep -q "Connected: yes"; then
-            echo "2" # Подключено устройство
+        # Ищем подключенные устройства
+        mapfile -t connected_devices < <(bluetoothctl devices Connected | awk '{print $2}')
+
+        if [ ${#connected_devices[@]} -gt 0 ]; then
+            # Берем первое подключенное устройство для отображения заряда
+            local dev="${connected_devices[0]}"
+            # Получаем процент заряда (ищем строку Battery Percentage)
+            local battery
+            battery=$(bluetoothctl info "$dev" | grep "Battery Percentage" | awk -F '[()]' '{print $2}')
+
+            echo "󰂰" # Подключено, но заряд неизвестен
         else
-            echo "3" # Включено, но не подключено
+            echo "󰂳" # Включен, но не подключен
         fi
     else
-        echo "4"
+        echo "󰂲"
     fi
 }
 
+# Первый запуск при старте eww
 get_status
 
-# Отслеживание изменений в реальном времени через bluetoothctl
-bluetoothctl monitor | while read -r; do
-    get_status
+# Мониторинг изменений (подключения устройств и изменения уровня батареи)
+bluetoothctl monitor | while read -r line; do
+    # Перерисовываем интерфейс, если изменились свойства устройства или статус подключения
+    if echo "$line" | grep -qE "PropertiesChanged|Device|Connected"; then
+        get_status
+    fi
 done
