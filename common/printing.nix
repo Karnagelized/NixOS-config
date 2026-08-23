5cae0f8daa8e3d837f6c01c2a4a561311538e6f5
Antonov Maksim <karnalize@mail.ru>
2026-08-23 20:54:33 +0500

    Обновлен модуль принтера
    - Добавлены драйверы для печати
    - Добавлены настройки для печати по интернету

--- common/printing.nix
+++ common/printing.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Включение печати через CUPS
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.brgenml1cupswrapper
+      pkgs.brgenml1lpr
+    ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
