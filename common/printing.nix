de927420622ec0c595ef2f75cef98f9c7a74ca78
Antonov Maksim <karnalize@mail.ru>
2026-08-27 16:19:12 +0500

    Обновлен модуль принтера
    - Добавлен драйвер для сканирования

--- common/printing.nix
+++ common/printing.nix
@@ -9,6 +9,24 @@
     ];
   };
 
+  # Добавляем поддержку сканера
+  hardware.sane = {
+    enable = true;
+    # Включаем драйвер Brother
+    brscan5 = {
+      enable = true;
+      netDevices = {
+        BrotherScanner = {
+          model = "DCP-9020CDW";
+          ip = "192.168.000.247";
+        };
+      };
+    };
+
+    # Сетевое сканирование
+    extraBackends = [ pkgs.sane-airscan ];
+  };
+
   services.avahi = {
     enable = true;
     nssmdns4 = true;
